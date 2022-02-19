57929787b56271aac0e8ed52f219b6a6b38471cc
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-02-19 12:47:24 +0530

    Time: 280 ms (25.40%), Space: 115.2 MB (25.40%) - LeetHub

--- 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
+++ 1675-minimize-deviation-in-array/1675-minimize-deviation-in-array.java
@@ -1,26 +1,35 @@
 class Solution {
     public int minimumDeviation(int[] nums) {
         
-        Queue<Integer>q=new PriorityQueue<>((a,b)->(b-a));
+        TreeSet<Integer>set=new TreeSet<>();
         
-        int min=Integer.MAX_VALUE;
+        
+       
         
         for(int i:nums){
             if(i%2==1) i=i*2;
-            min=Math.min(min,i);
-            q.add(i);
+            
+            set.add(i);
         }
         
         int diff=Integer.MAX_VALUE;
         
-        while(q.peek()%2==0){
-            int max=q.remove();
+        while(true){
+            int max=set.last();
+            int min=set.first();
+            
             diff=Math.min(diff,max-min);
-            min=Math.min(min,max/2);
-            q.add(max/2);
+            
+            if(max%2==0){
+                set.remove(max);
+                set.add(max/2);
+            }
+            else{
+                break;
+            }
         }
         
-        return Math.min(diff,q.peek()-min);
+        return diff;
         
     }
 }
