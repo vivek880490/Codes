0f90e8bf728758c1896739c3f8333e3964bb121f
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-02-20 13:56:06 +0530

    Time: 26 ms (76.57%), Space: 59.7 MB (25.24%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,36 +1,41 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        int n=nums.length;
         
         Arrays.sort(nums);
-        List<List<Integer>>al=new ArrayList<>();
+        List<List<Integer>>ans=new ArrayList<>();
         
-        for(int i=0;i<n-2;i++){
+        for(int i=0;i<nums.length-2;i++){
             if(i==0 || (i>0 && nums[i]!=nums[i-1])){
+                
                 int l=i+1;
-                int h=n-1;
-                int sum=0-nums[i];
+                int h=nums.length-1;
+                
+                int target=0-nums[i];
                 
                 while(l<h){
-                    if(nums[l]+nums[h]==sum){
+                    
+                    if(nums[l]+nums[h]==target){
                         ArrayList<Integer>temp=new ArrayList<>();
                         temp.add(nums[i]);
                         temp.add(nums[l]);
                         temp.add(nums[h]);
-                        al.add(temp);
                         
-                        
-                        while(l<h && nums[l]==nums[l+1])l++;
-                        while(l<h && nums[h]==nums[h-1])h--;
-                        l++;
-                        h--;
+                        ans.add(temp);
+                    
+                    
+                    while(l<h && nums[l]==nums[l+1]) l++;
+                    while(l<h && nums[h]==nums[h-1]) h--;
+                    
+                    l++;
+                    h--;
+                
                     }
-                    else if(nums[l]+nums[h]>sum) h--;
-                    else l++;
-                }
+                else if(nums[l]+nums[h]>target) h--;
+                else l++;
+                } 
             }
         }
-        return al;
+        return ans;
         
     }
 }
