ceedb6fd187b32322f4dfe5226afcd0bfed0f10b
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-02-21 10:52:01 +0530

    Time: 2 ms (55.40%), Space: 42.4 MB (15.37%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.java
+++ 844-backspace-string-compare/844-backspace-string-compare.java
@@ -1,47 +1,43 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
+        StringBuilder sb=new StringBuilder();
         
-        Stack<Character>st=new Stack<>();
-     
-        for(int i=0;i<s.length();i++){
+        for(int i=0;i<s.length();i++)
+        {
             char ch=s.charAt(i);
-            if(ch=='#' && st.size()>0){
-                st.pop();
+            if(ch=='#' & sb.length()>0){
+                sb.deleteCharAt(sb.length() - 1);
             }
             else if(ch=='#'){
-            continue;
+                continue;
             }
-            else{
-            st.push(ch);
+            else
+            {
+                sb.append(ch);
             }
         }
-            StringBuilder sb=new StringBuilder();
-            
-            while(st.size()>0){
-                sb.append(st.pop());
-            }
-            
-            
-             for(int i=0;i<t.length();i++){
+        
+        
+        
+        StringBuilder tb=new StringBuilder();
+        
+        for(int i=0;i<t.length();i++)
+        {
             char ch=t.charAt(i);
-            if(ch=='#' && st.size()>0){
-                st.pop();
+            if(ch=='#' & tb.length()>0){
+                tb.deleteCharAt(tb.length() - 1);
             }
             else if(ch=='#'){
-            continue;
+                continue;
             }
-                 else{
-            st.push(ch);
+            else
+            {
+                tb.append(ch);
+         
             }
-             }
-            StringBuilder tb=new StringBuilder();
-            
-            while(st.size()>0){
-                tb.append(st.pop());
-            }
-                  
-        System.out.println(sb.toString() +" "+ tb.toString());
+        }
         
-             return (sb.toString().equals(tb.toString())); 
+        return (sb.toString().equals(tb.toString()));
+        
     }
 }
