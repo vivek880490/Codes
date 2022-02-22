f79a5e6ef6632d0d9496c0f5f2b179d65b3b26ea
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-02-22 11:50:35 +0530

    Time: 12 ms (70.76%), Space: 47.1 MB (60.59%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,53 +1,48 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
-       
+        if(s.length()==0 || p.length()==0 || s.length()<p.length()){
+            return new ArrayList<Integer>();
+        }
         
-        ArrayList<Integer>al=new ArrayList<>();
+        List<Integer>al=new ArrayList<>();
         
-        if (s.length() == 0 || p.length() == 0 || s.length() < p.length()){
-           return new ArrayList<Integer>();
-       }
-       
-        int n=s.length();
-        int m=p.length();
+        int m=s.length();
+        int n=p.length();
         
-        int count[]=fre(p);
+        int pf[]=freq(p);
+        int sf[]=freq(s.substring(0,n));
         
-        int ccount[]=fre(s.substring(0,m));
+        if(same(pf,sf)) al.add(0);
         
-        if(same(count,ccount))
-            al.add(0);
-        
-       
-        
-        for(int i=m;i<n;i++){
-            ccount[s.charAt(i-m)-'a']--;
-            ccount[s.charAt(i)-'a']++;
+        for(int i=n;i<m;i++){
+            sf[s.charAt(i-n)-'a']--; //released 0th
+            sf[s.charAt(i)-'a']++; //aquired next
             
-            if(same(count,ccount))
-                al.add(i-m+1);
+            if(same(pf,sf)){
+                al.add(i-n+1);
+            }
             
         }
         return al;
         
-      
     }
     
-    boolean same(int count[],int ccount[]){
-        for(int i=0;i<26;i++){
-        if(count[i]!=ccount[i]){
-            return false;
-        }
+    boolean same(int pf[],int sf[]){
+        for(int i=0;i<pf.length;i++){
+            if(pf[i]!=sf[i]){
+                return false;
+            }
         }
         return true;
     }
     
-    int [] fre(String s){
-        int count[]=new int[26];
-        for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
+    
+    int [] freq(String str){
+        int fre[]=new int[26];
+        for(int i=0;i<str.length();i++){
+            fre[str.charAt(i)-'a']++;
         }
-        return count;
+        return fre;
     }
 }
