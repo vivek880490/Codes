2b8528920f5f66768d54b48361cf3a480668e52c
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-02-26 11:56:47 +0530

    Time: 21 ms (34.95%), Space: 41.8 MB (50.51%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -16,29 +16,31 @@
 class Solution {
     int count=0;
     public int pathSum(TreeNode root, int target) {
+        if(root == null) return 0;
         
-        HashMap<Integer,Integer>map=new HashMap<>();
-        map.put(0,1);
-        
-        solve(root,0,target,map);
+        solve(root,target,0);
+        pathSum(root.left,target);
+        pathSum(root.right,target);
         
         return count;
     }
     
-    void solve(TreeNode root,int cursum,int target,HashMap<Integer,Integer>map){
+    void solve(TreeNode root,int target,int cursum){
         if(root==null) return;
         
         cursum+=root.val;
         
-        if(map.containsKey(cursum-target)){
-            count+=map.get(cursum-target);
-        }
-        map.put(cursum,map.getOrDefault(cursum,0)+1);
+        if(cursum==target) count++;
         
-        solve(root.left,cursum,target,map);
-        solve(root.right,cursum,target,map);
-        map.put(cursum,map.getOrDefault(cursum,0)-1);
-        return;
+        // if(map.containsKey(cursum-target)){
+        //     count+=map.get(cursum-target);
+        // }
+        // map.put(cursum,map.getOrDefault(cursum,0)+1);
+        
+        solve(root.left,target,cursum);
+        solve(root.right,target,cursum);
+        //map.put(cursum,map.getOrDefault(cursum,0)-1);
+        // return;
         
         
     }
