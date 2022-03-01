7dc5989f71ca2b528e1a61ff7f93452df5a4d267
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-01 17:36:54 +0530

    Time: 4 ms (83.18%), Space: 44.4 MB (70.44%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.java
@@ -11,48 +11,30 @@
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         
-        if(lists.length == 0) return null;
+        PriorityQueue<ListNode>q=new PriorityQueue<>((a,b)->(a.val-b.val));
         
-        return mergeklist(lists,0,lists.length-1);
-        
-    }
-    // divide
-    ListNode mergeklist(ListNode lists[],int l,int h){
-        
-        if(l>h) return null;
-        if(l == h) return lists[l];
-        
-        int mid = (l+h)/2;
-        
-        ListNode l1 = mergeklist(lists,l,mid);
-        ListNode l2= mergeklist(lists,mid+1,h);
-        
-        return merge(l1,l2);
-    }
-    //merge two sorted linked list
-    ListNode merge(ListNode l1, ListNode l2){
-        if(l1==null) return l2;
-        if(l2==null) return l1;
+        //add all non null lists to queue
+        for(ListNode l:lists){
+            if(l!=null){
+                q.add(l);
+            }
+        }
         
         ListNode dummy = new ListNode(-1);
-        ListNode temp = dummy;
+        ListNode temp=dummy;
         
-        ListNode cur1 = l1;
-        ListNode cur2 = l2;
-        
-        while(cur1!=null && cur2!=null){
-            if(cur1.val<cur2.val){
-                temp.next=cur1;
-                cur1=cur1.next;
-            }
-            else{
-                temp.next=cur2;
-                cur2=cur2.next;
-            }
+        while(q.size()>0){
+            ListNode rem=q.poll();
+            
+            temp.next=rem;
             temp=temp.next;
+            rem=rem.next;
+            
+            if(rem!=null) q.add(rem);
         }
-        temp.next = cur1==null?cur2:cur1;
         
         return dummy.next;
+        
+        
     }
 }
