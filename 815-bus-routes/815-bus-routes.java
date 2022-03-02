7780477a800696311d8f8dd766b4c474dd0a0c51
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-02 13:55:27 +0530

    Time: 51 ms (88.34%), Space: 77.6 MB (75.43%) - LeetHub

--- 815-bus-routes/815-bus-routes.java
+++ 815-bus-routes/815-bus-routes.java
@@ -1,63 +1,65 @@
 class Solution {
-    public int numBusesToDestination(int[][] routes, int source, int target) {
+    public int numBusesToDestination(int[][] routes, int s, int t) {
         
-        int n=routes.length;
+    //create hasmap ki wo bus kis kis bus stop je jata hai
+        int n = routes.length;
+        HashMap<Integer,ArrayList>map=new HashMap<>();
         
-        HashMap<Integer,ArrayList<Integer>>map=new HashMap<>();
-        
-        for(int i=0;i<n;i++){
+        for(int i=0;i<routes.length;i++){
             for(int j=0;j<routes[i].length;j++){
                 
-                int busstopno=routes[i][j];
+                int busstopno = routes[i][j];
                 
-                ArrayList<Integer>busno=map.getOrDefault(busstopno,new ArrayList<>());
+                ArrayList<Integer>busno = map.getOrDefault(busstopno,new ArrayList<>());
+                
                 busno.add(i);
-                map.put(busstopno,busno);
+                
+                map.put(busstopno , busno);
                 
             }
         }
         
-        Queue<Integer>q=new LinkedList<>();
-        HashSet<Integer>busstopvis=new HashSet<>();
-        HashSet<Integer>busvis=new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
+        HashSet<Integer> busstopvis = new HashSet<>();
+        HashSet<Integer> busvis = new HashSet<>();
         
+        q.add(s);
+        busstopvis.add(s);
         int level=0;
-        q.add(source);
-        busstopvis.add(source);
         
         while(q.size()>0){
-            int size=q.size();
+            int size = q.size();
+            
             while(size-->0){
-                int node=q.poll();
                 
-                if(node==target){
-                    return level;
-                }
+                int rem = q.poll();
+                // we got our destination
+                if(rem == t) return level;
                 
-                ArrayList<Integer>buses=map.get(node);
+                ArrayList<Integer>buses = map.get(rem);
                 
-                for(int bus:buses){
+                
+                for(int bus : buses){
                     
                     if(busvis.contains(bus)) continue;
                     
-                    int arr[]=routes[bus];
+                    int arr[] = routes[bus];
                     
-                    for(int stop:arr){
+                    for(int stop : arr){
                         
                         if(busstopvis.contains(stop)) continue;
                         
                         q.add(stop);
                         busstopvis.add(stop);
-                        
                     }
                     busvis.add(bus);
-                    
                 }
             }
+            
             level++;
             
-        }
-        return -1;
+            }
         
+        return -1;
     }
 }
