0d1b4d8de5af71a797f8df4aae62dd45589c8745
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-02 10:06:17 +0530

    Time: 23 ms (45.55%), Space: 42.4 MB (86.39%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -1,17 +1,19 @@
 class Solution {
     public int jump(int[] nums) {
-        int n=nums.length;
+       
+       int n=nums.length;
         int dp[]=new int[n];
-        dp[n-1]=0;
+        // already on last index
+       dp[n-1]=0;
+        
         for(int i=n-2;i>=0;i--){
-            
             int steps=nums[i];
             int min=Integer.MAX_VALUE;
-            
+            //last index se pahle tak, and jump should not be out of array
             for(int j=i+1;j<n && j<=i+steps;j++){
                 min=Math.min(min,dp[j]);
             }
-            
+            //min is still same means there is no steps
             if(min==Integer.MAX_VALUE){
                 dp[i]=min;
             }
@@ -20,6 +22,5 @@ class Solution {
             }
         }
         return dp[0];
-        
     }
 }
