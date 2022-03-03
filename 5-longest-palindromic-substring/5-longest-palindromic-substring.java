0dfb3337ba04f3299cd6507e68aca1f1688814ac
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-03 10:23:49 +0530

    Time: 486 ms (20.07%), Space: 50.1 MB (38.84%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,42 +1,41 @@
 class Solution {
     public String longestPalindrome(String s) {
-        
+        String ans="";
         boolean dp[][]=new boolean[s.length()][s.length()];
-        int l=0;
-        int h=0;
+        // for gap that is diagnol
         for(int g=0;g<s.length();g++){
-            for(int i=0,j=g;j<s.length();i++,j++){
-                
+            for(int i = 0 , j = g ; j<s.length() ;i++,j++){
+                // one length string is always palindrome
                 if(g==0){
                     dp[i][j]=true;
                 }
+                // two length is palindrome of both character are same
                 else if(g==1){
                     if(s.charAt(i)==s.charAt(j)){
-                        dp[i][j]=true;
+                        dp[i][j] = true;
                     }
                     else{
-                        dp[i][j]=false;
+                        dp[i][j] = false;
                     }
                 }
                 else{
+                    // check if extreme means first and last character same same and middel are also palindrome
                     if(s.charAt(i)==s.charAt(j) && dp[i+1][j-1]==true){
-                        dp[i][j]=true;
+                        dp[i][j]  = true;
                     }
                     else{
-                        dp[i][j]=false;
+                        dp[i][j] = false;
                     }
+                    
                 }
-                 if(dp[i][j]==true){
-                     l=i;
-                     h=j;
-                     
+                // at every gap we will check for the len of valid substring that are plaindrome
+                if(dp[i][j]==true){
+                    ans = s.substring(i,j+1);
+                }
                 
             }
-                
-            }
-              
-         
-        }  
-        return s.substring(l,h+1);
+        }
+        
+        return ans;
     }
 }
