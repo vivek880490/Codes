ac1f71714b65bed4af42ba87fb105346e3899d9c
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-04 17:35:53 +0530

    Time: 12 ms (67.84%), Space: 44.4 MB (28.62%) - LeetHub

--- 773-sliding-puzzle/773-sliding-puzzle.java
+++ 773-sliding-puzzle/773-sliding-puzzle.java
@@ -1,67 +1,68 @@
 class Solution {
     public int slidingPuzzle(int[][] board) {
         
-        Queue<String>q=new LinkedList<>();
+        Queue<String>q = new LinkedList<>();
+        //target string given in question
+        String target = "123450";
         
-        String tar="123450";
-        
-        StringBuilder sb=new StringBuilder();
-        
+        //initial given board
+        StringBuilder initial = new StringBuilder(); 
         for(int i=0;i<board.length;i++){
-            for(int j=0;j<board[0].length;j++)
-            {
-                sb.append(board[i][j]);
+            for(int j=0;j<board[0].length;j++){
+                initial.append(board[i][j]);
             }
         }
-
-        String initial=sb.toString();
+        //visited set
+        HashSet<String>vis = new HashSet<>();
         
-        int swapidx[][]={{1,3},{0,2,4},{1,5},{0,4},{1,3,5},{4,2}};
+        q.add(initial.toString());
         
-        q.add(initial);
-        int level=0;
-        HashSet<String>vis=new HashSet<>();
+        int level = 0;
+        //on each index with whom i can swap like dirs array
+        int swapidx[][] = {{1,3},{0,2,4},{1,5},{0,4},{1,3,5},{4,2}};
         
         while(q.size()>0){
-            int size=q.size();
+            int size = q.size();
+            
             while(size-->0){
-                String rem=q.poll();
-                
-                if(rem.equals(tar)) return level;
-                
-                int idx=-1;
-                
-                for(int i=0;i<rem.length();i++){
-                    if(rem.charAt(i)=='0'){
-                        idx=i;
-                        break;
-                    }
+            
+            String rem = q.poll();
+            
+            if(rem.equals(target)) return level;
+            // find the index of 0 in removed string
+            int idx = -1;
+            for(int i = 0; i< rem.length(); i++){
+                if(rem.charAt(i)=='0'){
+                    idx = i;
+                    break;
                 }
+            }
+            
+            int swap[] = swapidx[idx];
+            
+            for(int i = 0; i<swap.length;i++){
                 
-                int swap[]=swapidx[idx];
+                String tobeadded = swapchar(rem,idx,swap[i]);
                 
-                for(int i=0;i<swap.length;i++){
-                    String tobeadded=swapchar(rem,idx,swap[i]);
-                    
-                    if(vis.contains(tobeadded)) continue;
-                    
-                    q.add(tobeadded);
-                    vis.add(tobeadded);
-                }
-             
+                if(vis.contains(tobeadded)) continue;
+                q.add(tobeadded);
+                vis.add(tobeadded);
+            }
             }
             level++;
-   
         }
         return -1;
+        }
         
-    }
+        String swapchar(String rem,int i,int j){
+            StringBuilder sb = new StringBuilder(rem);
+            
+            sb.setCharAt(i,rem.charAt(j));
+            sb.setCharAt(j,rem.charAt(i));
+            
+            return sb.toString();
+        }
+        
+        
     
-    String swapchar(String rem,int i,int j){
-        
-        StringBuilder sb=new StringBuilder(rem);
-        sb.setCharAt(i,rem.charAt(j));
-        sb.setCharAt(j,rem.charAt(i));
-        return sb.toString();
-    }
 }
