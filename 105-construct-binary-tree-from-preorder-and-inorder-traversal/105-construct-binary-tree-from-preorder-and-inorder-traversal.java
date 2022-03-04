b314b0d6af661cf0ca6d4709c351c75aebedf338
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-04 13:24:29 +0530

    Time: 5 ms (38.04%), Space: 44.4 MB (32.41%) - LeetHub

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -16,32 +16,25 @@
 class Solution {
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         
-        int n=preorder.length;
-        return solve(preorder,0,n-1,inorder,0,n-1);
+        int n = preorder.length;
         
+        return solve(preorder,0,n-1,inorder,0,n-1);
     }
     
     TreeNode solve(int preorder[],int psi,int pei,int inorder[],int isi,int iei){
+        if(isi > iei) return null;
         
-        if(isi>iei){
-            return null;
-        }
-        
-        int idx=isi;
+        int idx = isi;
         while(preorder[psi]!=inorder[idx]){
             idx++;
         }
+        int len = idx-isi;
         
-        int colst=idx-isi;
+        TreeNode root = new TreeNode(preorder[psi]);
         
-        TreeNode node=new TreeNode(preorder[psi]);
+        root.left = solve(preorder,psi+1,psi+len,inorder,isi,idx-1);
+        root.right = solve(preorder,psi+len+1,pei,inorder,idx+1,iei);
         
-        node.left=solve(preorder,psi+1,psi+colst,inorder,isi,idx-1);
-        node.right=solve(preorder,psi+colst+1,pei,inorder,idx+1,iei);
-        
-        return node;
-        
+        return root;
     }
-    
-    
 }
