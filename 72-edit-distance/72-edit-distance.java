a4333ef51a18a23c80806230c6f3f9e723660bb4
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-07 09:33:36 +0530

    Time: 7 ms (74.52%), Space: 44.7 MB (41.08%) - LeetHub

---
+++ 72-edit-distance/72-edit-distance.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minDistance(String s1, String s2) {
+        
+        int dp[][] = new int [s1.length()+1][s2.length()+1];
+        
+        for(int i=0;i<dp.length;i++){
+            dp[i][0] = i;
+        }
+        for(int i=1; i<dp[0].length; i++){
+            dp[0][i] = i;
+        }
+        
+        for(int i=1;i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+                if(s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.min(dp[i-1][j-1],Math.min(dp[i-1][j],dp[i][j-1]))+1;
+                }
+            }
+        }
+        return dp[dp.length-1][dp[0].length-1];
+        
+        
+    }
+}
