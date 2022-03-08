04801ae562dc154e778b14d5e89dda0460f809ab
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-08 11:45:38 +0530

    Time: 48 ms (9.80%), Space: 64.5 MB (5.47%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -6,24 +6,24 @@ class Solution {
         
         Integer qb[][] = new Integer[n+1][m+1];
         
-        return solve(text1, text2, n, m,n-1,m-1,0,qb);
+        return solve(text1, text2, n, m,qb);
     }
     
-    int solve(String text1, String text2, int n, int m,int i, int j,int ans,Integer qb[][]){
+    int solve(String text1, String text2, int n, int m,Integer qb[][]){
         
         if(n==0 || m==0 ) return 0;
         
-        if(qb[i][j] != null) return qb[i][j];
+        if(qb[n-1][m-1] != null) return qb[n-1][m-1];
         
-        if(text1.charAt(i) == text2.charAt(j)){
-            qb[i][j] = 1 + solve(text1, text2, n-1, m-1, i-1, j-1,ans,qb);
+        if(text1.charAt(n-1) == text2.charAt(m-1)){
+            qb[n-1][m-1] = 1 + solve(text1, text2, n-1, m-1,qb);
         }
         
         else{
-            qb[i][j] = Math.max(solve(text1, text2, n-1,m,i-1,j,ans,qb),solve(text1, text2, n, m-1,i,j-1,ans,qb));
+            qb[n-1][m-1] = Math.max(solve(text1, text2, n-1,m,qb),solve(text1, text2,n,m-1,qb));
         }
         
-        return qb[i][j];
+        return qb[n-1][m-1];
         
     }
     
