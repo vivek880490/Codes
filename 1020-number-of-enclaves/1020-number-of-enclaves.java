5d516c12aadbb3b6321eb5f0bddc9bbf3ae2a684
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-10 11:26:06 +0530

    Time: 7 ms (53.68%), Space: 65.3 MB (52.31%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,41 +1,53 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-        int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(i==0 || j==0 || i==grid.length-1 || j==grid[0].length-1 ){
+        
+        
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                
+                if(i==0){
                     if(grid[i][j]==1){
-                    dfs(grid,i,j);
+                        dfs(grid,i,j);
+                    }
                 }
+                else if(j==0){
+                    if(grid[i][j]==1){
+                        dfs(grid,i,j);
+                    }
+                }
+                else if(i==grid.length-1){
+                    if(grid[i][j]==1){
+                        dfs(grid,i,j);
+                    }
+                }
+                else if(j==grid[0].length-1){
+                    if(grid[i][j]==1){
+                        dfs(grid,i,j);
+                    }
                 }
             }
         }
-        
-         for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
-                    count++;
-                }
-                
+        int count = 0;
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j]==1) count++;
             }
-         }
+        }
+        
         return count;
         
-        
     }
-    int dirs[][]={{0,-1},{-1,0},{0,1},{1,0}};
-    void dfs(int grid[][],int r,int c){
-        grid[r][c]=0;
-        for(int i=0;i<4;i++){
-            int rd=r+dirs[i][0];
-            int cd=c+dirs[i][1];
-            
-            if(rd<0 || cd<0 || rd>=grid.length || cd>=grid[0].length || grid[rd][cd]==0){
-                continue;
-            }
-            grid[rd][cd]=0;
-            dfs(grid,rd,cd);
+    
+    void dfs(int grid[][],int i, int j){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]==0){
+            return;
         }
         
+        grid[i][j] = 0;
+        
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
     }
 }
