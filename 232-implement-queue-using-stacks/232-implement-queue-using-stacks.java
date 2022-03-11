d7e31c6fd77e26c0c95ef7d933ec15895c215892
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-11 23:22:17 +0530

    Time: 1 ms (56.22%), Space: 41.8 MB (39.51%) - LeetHub

---
+++ 232-implement-queue-using-stacks/232-implement-queue-using-stacks.java
@@ -0,0 +1,39 @@
+class MyQueue {
+     Stack<Integer> input;
+     Stack<Integer> output;
+
+    public MyQueue() {
+       input = new Stack<>();
+       output = new Stack<>();
+    }
+    
+    public void push(int x) {
+       // only push the new item when the order is maintained as precondition to push
+        while(!output.isEmpty()) {
+		    input.push(output.pop());
+        }
+        input.push(x);
+    }
+    
+    public int pop() {
+         // all contents of input -> output as a precondition to pop
+        while(!input.isEmpty()) {
+            output.push(input.pop());
+        }
+
+        return output.pop();	
+    }
+    
+    public int peek() {
+        // all contents of input -> output as a precondition to pop
+        while(!input.isEmpty()) {
+            output.push(input.pop());
+        }
+
+        return output.peek();
+    }
+    
+    public boolean empty() {
+        return input.isEmpty() && output.isEmpty();
+    }
+}
