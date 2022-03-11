7b2e497b24c0877d989e4e5a34831fa424116412
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-11 09:03:21 +0530

    Time: 26 ms (53.30%), Space: 65.3 MB (23.66%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.java
@@ -1,41 +1,44 @@
 class Solution {
     public int maxDistance(int[][] grid) {
-        Queue<int []>q=new LinkedList<>();
-        int count=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]==1){
+        
+        int count = 0;
+        Queue<int []> q = new LinkedList<>();
+        for(int i=0; i<grid.length; i++){
+            for(int j=0; j<grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    q.add(new int[]{i,j});
                     count++;
-                    q.add(new int []{i,j});
                 }
             }
         }
-        // ek bhi 1 nahi hai ya sara 1 hai tab nearest distance kya hi hoga
-        if(count==0 || count==grid.length*grid[0].length) return -1;
+        if(count == 0) return -1;
+        if(count == grid.length*grid[0].length) return -1;
         
-        int level=-1;
-        int dirs[][]={{-1,0},{0,-1},{1,0},{0,1}};
+        int level = -1;
+        int dirs[][]={{0,1},{1,0},{0,-1},{-1,0}};
         
-        while(q.size()>0){
-            int size=q.size();
+        while(q.size() > 0){
+            int size = q.size();
             level++;
-            
             while(size-->0){
-                // poll first element to run bfs
-                int rem[]=q.poll();
+                int rem[] = q.poll();
                 
-                for(int i=0;i<4;i++){
-                    int rd=rem[0]+dirs[i][0];
-                    int cd=rem[1]+dirs[i][1];
+                for(int i=0; i<4; i++){
+                    int dx = dirs[i][0]+rem[0];
+                    int dy = dirs[i][1]+rem[1];
                     
-                    if(rd<0 || cd<0 || rd>=grid.length || cd>=grid[0].length || grid[rd][cd]==1) continue;
+                    if(dx<0 || dy<0 || dx >= grid.length || dy >= grid[0].length || grid[dx][dy]==1) continue;
                     
-                    q.add(new int[]{rd,cd});
-                    grid[rd][cd]=1;
+                    
+                    q.add(new int []{dx,dy});
+                    grid[dx][dy]=1;
+                    
+                    
+                    
                 }
-                
             }
         }
+        
         return level;
     }
 }
