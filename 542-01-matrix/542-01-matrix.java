4a3e4069b349974ad2220aab44ae7da6bce2dc7c
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-12 18:27:56 +0530

    Time: 27 ms (33.62%), Space: 72.3 MB (60.86%) - LeetHub

--- 542-01-matrix/542-01-matrix.java
+++ 542-01-matrix/542-01-matrix.java
@@ -1,32 +1,33 @@
 class Solution {
-    int dirs[][]={{0,1},{0,-1},{-1,0},{1,0}};
     public int[][] updateMatrix(int[][] mat) {
         
-        Queue<int []>q=new LinkedList<>();
+        Queue<int[]> q= new LinkedList<>();
         
-        for(int i=0;i<mat.length;i++){
-            for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]==0){
+        for(int i=0; i<mat.length; i++){
+            for(int j=0; j<mat[0].length; j++){
+                if(mat[i][j] == 0){
                     q.add(new int[]{i,j});
-                    
                 }
                 else{
-                    mat[i][j]=-1;
+                    mat[i][j] = -1;
                 }
             }
         }
         
+        int dirs[][] ={{0,1},{1,0},{0,-1},{-1,0}};
+        
         while(q.size()>0){
-            int rem[]=q.poll();
+            int rem[] = q.poll();
             
-            for(int i=0;i<4;i++){
-                int rd=rem[0]+dirs[i][0];
-                int cd=rem[1]+dirs[i][1];
+            for(int i=0; i<4; i++){
+                int dx = rem[0]+dirs[i][0];
+                int dy = rem[1]+dirs[i][1];
                 
-                if(rd>=0 && cd>=0 && rd<mat.length && cd<mat[0].length && mat[rd][cd]<0){
+                if(dx>=0 && dy>=0 && dx < mat.length && dy < mat[0].length && mat[dx][dy]<0){
                     
-                    mat[rd][cd]=mat[rem[0]][rem[1]]+1;
-                    q.add(new int[]{rd,cd});
+                    mat[dx][dy]=mat[rem[0]][rem[1]]+1;
+                    
+                    q.add(new int[]{dx,dy});
                 }
             }
         }
