bafa51f9326c1c6bef3d1897adaecee781f10efa
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-12 11:03:57 +0530

    Time: 13 ms (6.33%), Space: 43.3 MB (5.50%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.java
+++ 392-is-subsequence/392-is-subsequence.java
@@ -1,23 +1,25 @@
 class Solution {
-    public boolean isSubsequence(String s, String t) {
-        if(s.isEmpty()) return true;
-        if((s==null) && t == null) return true;
-        if(s==null || t == null) return false;
-        return solve(0,0,s,t);
+    public boolean isSubsequence(String a, String b) {
         
-    }
+        //if(s.length() > t.length()) return false;
+        
+        
+        int dp[][] = new int[a.length()+1][b.length()+1];
+        
+        for(int i=1; i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+                if(a.charAt(i-1) == b.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+            
+        }
+        
+    int len = dp[dp.length-1][dp[0].length-1];
     
-    boolean solve(int i,int j,String s,String t){
-        if(i==s.length()) return true;
-        
-        if(i==s.length() || j==t.length()) return false;
-        
-        if(s.charAt(i)==t.charAt(j))
-            return  solve(i+1,j+1,s,t);
-        
-        else
-            return solve(i,j+1,s,t);
-        
-    
+    return len==a.length()?true:false;
     }
 }
