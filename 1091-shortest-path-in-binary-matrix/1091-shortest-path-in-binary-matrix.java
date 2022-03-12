3c2c1c67892e9cf0544bc491a1b8b751fb0a1c0b
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-12 18:06:51 +0530

    Time: 10 ms (99.08%), Space: 43.6 MB (85.57%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -1,43 +1,37 @@
 class Solution {
     public int shortestPathBinaryMatrix(int[][] grid) {
+        int n = grid.length;
         
-        if(grid[0][0]==1) return -1;
+        int m = grid[0].length;
         
-        int m=grid.length;
-        int n=grid[0].length;
+        if(grid[0][0] == 1 ) return -1;
         
-        Queue<int[]>q=new LinkedList<>();
-        // add starting posn
+        Queue<int[]>  q = new LinkedList<>();
+        
         q.add(new int[]{0,0,1});
-        //mark starting position visited
-        grid[0][0]=1;
-        int dirs[][]={{0,1},{0,-1},{1,0},{-1,0},{1,1},{-1,-1},{-1,1},{1,-1}};
-        while(q.size()>0){
-            int size=q.size();
+        
+        grid[0][0] = 1;
+        
+        int dirs[][] = {{0,1},{1,0},{0,-1},{-1,0},{-1,-1},{1,1},{1,-1},{-1,1}};
+        
+        while(q.size() > 0){
+            int rem[] = q.poll();
             
-            while(size-->0){
-                int rem[]=q.poll();
+            if(rem[0] == n-1 && rem[1] == m-1) return rem[2];
+            
+            
+            for(int i=0; i<8; i++){
+                int dx = rem[0]+dirs[i][0];
+                int dy = rem[1]+dirs[i][1];
                 
-                if(rem[0]==m-1 && rem[1]==n-1) return rem[2];
-                
-                for(int dir[]:dirs){
-                    int r=rem[0]+dir[0];
-                    int c=rem[1]+dir[1];
-                    
-                    if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==0){
-                        q.add(new int[]{r,c,rem[2]+1});
-                        grid[r][c]=1;
-                    }
+                if(dx >= 0 && dy >=0 && dx < grid.length && dy < grid[0].length && grid[dx][dy] == 0){
+                    grid[dx][dy] = 1;
+                    q.add(new int[]{dx,dy,rem[2]+1});
                 }
-                
-                
             }
         }
+        
         return -1;
-        
-        
-        
-        
         
     }
 }
