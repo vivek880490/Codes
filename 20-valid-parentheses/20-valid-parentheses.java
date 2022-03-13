d9a59a6192d1564095b341b3fcad3670e6c451c8
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-13 10:28:00 +0530

    Time: 2 ms (81.09%), Space: 42.4 MB (18.20%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -6,19 +6,25 @@ class Solution {
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             
-            if(ch == '('){
-                st.push(')');
+            if(ch=='(' || ch=='[' || ch=='{'){
+                st.push(ch);
             }
-           else if(ch == '{'){
-                st.push('}');
+             else if(ch == ')' &&st.size() >0 && st.peek() == '('   ){
+                        st.pop();   
+                }
+            else if(ch == ']' &&st.size() >0 && st.peek() == '['   ){
+                        st.pop();   
+                }
+            else if(ch == '}' &&st.size() >0 && st.peek() == '{'   ){
+                        st.pop();   
+                }
+               
+            else{
+                return false;
             }
-            else if(ch == '['){
-                st.push(']');
+                
             }
-            
-            else if(st.size() == 0 || st.pop() != ch) return false;
-            
-        }
+        
         
         return (st.size()==0);
         
