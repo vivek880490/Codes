6587816be25470ba97c1abe53029daa31be46940
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-14 22:37:29 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (44.13%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -2,23 +2,16 @@ class Solution {
     public int fib(int n) {
         
         if(n == 0) return 0;
-        if(n == 1) return 1;
+        if(n==1) return 1;
+      int dp[] = new int[n+1];
         
-        Integer qb[] = new Integer[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
         
-        return solve(n,qb);
-    }
-    
-    int solve(int n, Integer qb[]){
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
         
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        
-        if(qb[n] != null) return qb[n];
-        
-        
-        int ans = solve(n-1,qb)+solve(n-2,qb);
-        
-        return qb[n] = ans;
+        return dp[n];
     }
 }
