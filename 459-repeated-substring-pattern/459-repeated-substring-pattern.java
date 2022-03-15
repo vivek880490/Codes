65c1dcea60842b972fe529961c60d8d4c10f8551
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-15 21:32:43 +0530

    Time: 53 ms (51.23%), Space: 67.5 MB (28.91%) - LeetHub

--- 459-repeated-substring-pattern/459-repeated-substring-pattern.java
+++ 459-repeated-substring-pattern/459-repeated-substring-pattern.java
@@ -1,11 +1,28 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         
-        String str = s+s;
+        int n = s.length();
         
-        return str.substring(1, str.length()-1).contains(s);
-        
-        // a baab a
-        // a bababa b
+        for(int i=n/2; i >= 1; i--){
+            if(n % i == 0){
+                int repeat = n/i;
+                
+                String substring = s.substring(0,i);
+                
+                StringBuilder sb = new StringBuilder();
+                
+                for(int j = 0; j<repeat; j++){
+                    
+                    sb.append(substring);
+                }
+                if(sb.toString().equals(s)) return true;
+            }
+            
+            
+            
+            
+        }
+        return false;
+            
     }
 }
