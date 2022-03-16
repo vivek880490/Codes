7cd92e4a29a1ed670c1682dd2bcf4217ac8a4643
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-16 17:02:44 +0530

    Time: 1 ms (20.00%), Space: 41.4 MB (47.67%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,22 +1,23 @@
 class Solution {
+    
     public int rob(int[] nums) {
-        
-        Integer qb[]=new Integer[nums.length];
-        return solve(0,nums,qb);
-        
+        int n = nums.length;
+       Integer qb[] = new Integer[n+1];
+        return solve(nums, 0, qb);
     }
     
-    int solve(int index,int nums[],Integer dp[]){
-       
-        if(index>=nums.length) return 0;
-         if(dp[index]!=null) return dp[index];
-        // lootenge tab next call next ka next elemet ke liye lagayenge
-        int loot=nums[index]+solve(index+2,nums,dp);
-        //skip karenge tab next elemnet se call lagayenge
-        int skip=solve(index+1,nums,dp);
+    int solve(int nums[], int i,Integer qb[]){
+        if(i >= nums.length) return 0;
         
-        dp[index]=Math.max(loot,skip);
+        if(qb[i] != null) return qb[i];
         
-        return dp[index];
+        int loot = nums[i]+solve(nums,i+2,qb);
+        int skip = solve(nums,i+1,qb);
+        
+        int temp = Math.max(loot,skip);
+        
+         //max = Math.max(temp,max);
+        
+        return qb[i]=temp;
     }
 }
