ad7e42fb7db6f31a7521ef24317cc05d1f5b1658
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-16 21:17:13 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (33.66%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,33 +1,27 @@
 class Solution {
     public int rob(int[] nums) {
         
-        if(nums.length==1) return nums[0];
-        Integer qb[]=new Integer[nums.length+1];
-        Integer qb1[]=new Integer[nums.length+1];
-        int s1 = solve(nums,1,qb);
-        int s2 = solve1(nums,0,qb1);
+        if(nums.length == 1) return nums[0];
         
-        return Math.max(s1,s2);
-
+        Integer qb1[] = new Integer[nums.length+1];
+        Integer qb2[] = new Integer[nums.length+1];
+        int n = nums.length;
+        int s1 = solve1(nums, 0,n-1, qb1);
+        int s2 = solve1(nums, 1,n, qb2);
+        
+        return Math.max(s1, s2);
     }
     
-    int solve(int nums[], int i,Integer qb[]){
-        if(i>=nums.length) return 0;
-        if(qb[i]!=null) return qb[i];
+    int solve1(int nums[], int i,int n, Integer qb[]){
+        if(i >= n) return 0;
+        if(qb[i] != null) return qb[i];
+        int rob = nums[i] + solve1(nums,i+2,n,qb);
+        int skip = solve1(nums, i+1, n, qb);
         
-        int loot = nums[i]+solve(nums,i+2,qb);
-        int skip = solve(nums,i+1,qb);
+        int temp = Math.max(rob, skip);
         
-        return qb[i]=Math.max(loot,skip);
-     }
+        return qb[i] = temp;
+    }
     
-     int solve1(int nums[], int i,Integer qb1[]){
-        if(i>=nums.length-1) return 0;
-        if(qb1[i]!=null) return qb1[i];
-        
-        int loot = nums[i]+solve1(nums,i+2,qb1);
-        int skip = solve1(nums,i+1,qb1);
-        
-        return qb1[i] = Math.max(loot,skip);
-     }
+    
 }
