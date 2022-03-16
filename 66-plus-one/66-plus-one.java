8cbe6ea049c8ada41ee758cde11cdbd3bcd867b7
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-16 22:28:17 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (20.92%) - LeetHub

--- 66-plus-one/66-plus-one.java
+++ 66-plus-one/66-plus-one.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] plusOne(int[] digits) {
         
-        for(int i=digits.length-1;i>=0;i--){
-            if(digits[i]<9){
+        for(int i=digits.length-1; i>=0; i--){
+            if(digits[i] < 9){
                 digits[i]++;
                 return digits;
             }
-            digits[i]=0;
-        }
+            digits[i] = 0;
+            }
         
-        int newn[]=new int[digits.length+1];
-        newn[0]=1;
-        return newn;
+        int newint[] = new int[digits.length+1];
+        
+        newint[0] = 1;
+        
+        return newint;
         
     }
 }
