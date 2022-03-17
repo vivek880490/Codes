3c3ce595bab95cbfe4fd6a5e5b1e8568daa40725
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-17 17:09:50 +0530

    Time: 133 ms (5.40%), Space: 74.3 MB (5.09%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.java
+++ 740-delete-and-earn/740-delete-and-earn.java
@@ -1,25 +1,28 @@
 class Solution {
     public int deleteAndEarn(int[] nums) {
-        
-        int inc = 0;
-        int exc = 0;
-        
-        int arr[] = new int [100001];
     
-        for(int x:nums){
-            arr[x]++;
-            
+        int arr[] = new int[100000];
+        
+        // repeating value hai to usko add karde rahe ek ghar bana de rahe hain
+        
+        for(int x : nums){
+            arr[x] += x;
         }
+        Integer qb[] = new Integer[100000];
+        return solve(arr, 0,qb);
+    
+    }
+    int solve(int arr[], int i, Integer qb[]){
         
-        for(int i=0;i<arr.length;i++){
-            int in = exc+arr[i]*i;
-            int ex = Math.max(inc,exc);
-            
-            inc = in;
-            exc = ex;
-        }
+        if(i >= arr.length) return 0;
         
-        return Math.max(inc,exc);
+        if(qb[i] != null) return qb[i];
         
+        int rob = arr[i] + solve(arr, i+2, qb);
+        int skip = solve(arr, i+1, qb);
+        
+        int temp = Math.max(rob, skip);
+        
+        return qb[i] = temp;
     }
 }
