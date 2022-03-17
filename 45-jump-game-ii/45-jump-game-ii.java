643b48602d3f987bafc680cd4589acbd67eedd0e
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-17 16:43:25 +0530

    Time: 227 ms (11.86%), Space: 50.5 MB (6.60%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.java
+++ 45-jump-game-ii/45-jump-game-ii.java
@@ -2,19 +2,24 @@ class Solution {
    
        
          public int jump(int[] nums) {
-		 dp=new Integer[nums.length+5];
-		 return minJumps(nums, 0);
+             
+		 Integer qb[] = new Integer[nums.length+5];
+		 return minJumps(nums, 0,qb);
 	 }
-	 Integer[] dp;
-	 int minJumps(int[] a,int i) {
-		 if(i>=a.length) return (Integer.MAX_VALUE-5);
-		 if(i==a.length-1) return 0;
-		 if(dp[i]!=null) return dp[i];
+    
+	 int minJumps(int[] a,int i, Integer qb[]) {
+         
+		 if(i >= a.length) return (Integer.MAX_VALUE-5);
+         
+		 if(i == a.length-1) return 0;
+         
+		 if(qb[i] != null) return qb[i];
 		 
-		 int jump=Integer.MAX_VALUE-5;
-		 for(int j=1;j<=a[i];j++) {
-			 jump=Math.min(jump,1+ minJumps(a, i+j));
+		 int jump = Integer.MAX_VALUE-5;
+         
+		 for(int j = 1;j <= a[i]; j++) {
+			 jump=Math.min(jump,1 + minJumps(a, i+j,qb));
 		 }
-		 return dp[i]=jump;
+		 return qb[i]=jump;
 	 }
 }
