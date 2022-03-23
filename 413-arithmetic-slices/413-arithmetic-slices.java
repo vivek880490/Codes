13023eafd4308dc4012a15683601caa35a5034e9
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-23 09:24:10 +0530

    Time: 0 ms (100.00%), Space: 42.2 MB (30.07%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,23 +1,24 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
+        int ans = 0;
         
+        if(nums.length >= 3){
         
-        if(nums.length<3) return 0;
-        int ans=0;
+        int dp[] = new int[nums.length];
         
-        int dp[]=new int[nums.length];
-        
-        for(int i=2;i<dp.length;i++){
-            if(nums[i]-nums[i-1]==nums[i-1]-nums[i-2]){
-                dp[i]=dp[i-1]+1;
-                ans+=dp[i];
+            for(int i=2; i<nums.length; i++){
+                if(nums[i] - nums[i-1] == nums[i-1]-nums[i-2]){
+                    
+                    dp[i] = dp[i-1]+1;
+                    ans+=dp[i];
+                }
             }
-            else{
-                dp[i]=0;
-            }
+            
+        
+        
+        
         }
+        
         return ans;
-        
-        
     }
 }
