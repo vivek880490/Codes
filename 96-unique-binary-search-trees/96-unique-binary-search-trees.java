ee1a5e124955ee17e032d458ea4fc9a307469087
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-24 10:17:59 +0530

    Time: 1 ms (15.21%), Space: 41.5 MB (8.57%) - LeetHub

--- 96-unique-binary-search-trees/96-unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/96-unique-binary-search-trees.java
@@ -1,22 +1,24 @@
 class Solution {
     public int numTrees(int n) {
         
-        int dp[] = new int [n+1];
+        HashMap<Integer, Integer> map = new HashMap<>();
         
-        dp[0] = 1;
-        dp[1] = 1;
+        map.put(0, 1);
+        map.put(1, 1);
         
-        for(int i=2; i<=n; i++){
-            int l = 0;
-            int r = i-1;
-            
-            while(l <= i-1){
-                dp[i] += dp[l]*dp[r];
-                l++;
-                r--;
-            }
+        return solve(n, map);
+    }
+    
+    int solve(int n, HashMap<Integer, Integer> map){
+        if(n == 0) return 1;
+        
+        if(map.containsKey(n)) return map.get(n);
+        
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+            sum += solve(i-1, map) * solve(n-i, map);
+            map.put(n, sum);
         }
-        
-        return dp[n];
+        return sum;
     }
 }
