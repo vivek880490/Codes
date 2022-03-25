fe884b47f73eb454ca88402f6e79a500acdabebc
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-25 10:33:39 +0530

    Time: 12 ms (69.96%), Space: 47 MB (75.04%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,48 +1,50 @@
 class Solution {
+    
     public List<Integer> findAnagrams(String s, String p) {
-        
-        if(s.length()==0 || p.length()==0 || s.length()<p.length()){
-            return new ArrayList<Integer>();
-        }
-        
-        List<Integer>al=new ArrayList<>();
-        
-        int m=s.length();
-        int n=p.length();
-        
-        int pf[]=freq(p);
-        int sf[]=freq(s.substring(0,n));
-        
-        if(same(pf,sf)) al.add(0);
-        
-        for(int i=n;i<m;i++){
-            sf[s.charAt(i-n)-'a']--; //released 0th
-            sf[s.charAt(i)-'a']++; //aquired next
             
-            if(same(pf,sf)){
-                al.add(i-n+1);
+        List<Integer> ans = new ArrayList<>();
+        
+        if(s.length() == 0 || p.length() == 0 || s.length() < p.length()) return ans;
+         
+        int m = s.length();
+        int n = p.length();
+        
+        int pf[] = freq(p);
+        int sf[] = freq(s.substring(0, n));
+        
+        if(same(pf,sf)) ans.add(0);
+            
+        for(int i=n; i<m; i++){
+            sf[s.charAt(i-n)-'a']--;
+            sf[s.charAt(i)-'a']++;
+            
+            if(same(sf, pf)){
+                ans.add(i-n+1);
             }
-            
         }
-        return al;
-        
+        return ans;
     }
     
-    boolean same(int pf[],int sf[]){
-        for(int i=0;i<pf.length;i++){
-            if(pf[i]!=sf[i]){
+    int[] freq(String s){
+        
+        int freq[] = new int[26];
+        
+        for(int i=0; i<s.length(); i++){
+         char ch = s.charAt(i);
+            freq[ch-'a']++;
+        }
+        
+        return freq;
+    }
+    
+    boolean same(int f1[], int f2[]){
+        
+        for(int i=0; i<f1.length; i++){
+            if(f1[i] != f2[i]){
                 return false;
             }
         }
         return true;
-    }
-    
-    
-    int [] freq(String str){
-        int fre[]=new int[26];
-        for(int i=0;i<str.length();i++){
-            fre[str.charAt(i)-'a']++;
-        }
-        return fre;
+        
     }
 }
