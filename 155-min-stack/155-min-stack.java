5e478fd0dd0aa8a7b9f008774f632f4cbe22e699
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-25 23:45:31 +0530

    Time: 9 ms (35.69%), Space: 48.5 MB (41.88%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,34 +1,59 @@
 class MinStack {
-	private Node head;
+    
+    Stack<Integer> st;
+    Stack<Integer> ss;
+
+    public MinStack() {
+        st = new Stack<>();
+        ss = new Stack<>();
+    }
+    
+    public void push(int val) {
         
-    public void push(int x) {
-        if (head == null) 
-            head = new Node(x, x, null);
-        else 
-            head = new Node(x, Math.min(x, head.min), head);
+        st.push(val);
+        
+        if(ss.size() == 0 || ss.peek() >= val){
+            ss.push(val);
+            return;
+        }
+        
     }
     
     public void pop() {
-        head = head.next;
+        int top = st.peek();
+        if(st.size() != 0){
+            
+            st.pop();
+        }
+        
+        if(top == ss.peek()){
+            ss.pop();
+        }
+        
     }
     
     public int top() {
-        return head.val;
+        
+        
+        if(st.size() == 0) return -1;
+        return st.peek();
+        
     }
     
     public int getMin() {
-        return head.min;
-    }
-        
-    private class Node {
-        int val;
-        int min;
-        Node next;
-            
-        private Node(int val, int min, Node next) {
-            this.val = val;
-            this.min = min;
-            this.next = next;
+        if(ss.size() == 0){
+            return -1;
         }
+        return ss.peek();
+        
     }
 }
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
