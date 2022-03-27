d44c18aa5c3cb9e339cbe1e68b71dbb48b35c7d2
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-27 17:15:10 +0530

    Time: 0 ms (100.00%), Space: 47.4 MB (40.41%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,55 +1,48 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
-        int ans[]=new int[2];
+        int ans[] = new int[2];
         
-        ans[0]=ans[1]=-1;
+        ans[0] = -1;
+        ans[1] = -1;
         
-        int l=0;
-        int h=nums.length-1;
+        int l = 0;
+        int h = nums.length-1;
         
-        while(l<=h){
+        while(l <= h){
+            int mid = l + (h-l)/2;
             
-            int mid=(l+h)/2;
-            
-            if(nums[mid]==target){
-                ans[0]=mid;
-                h=mid-1;
-                
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else {
-                h=mid-1;
+            if(nums[mid] == target){
+                ans[0] = mid;
+                h = mid-1;
             }
             
+            else if(nums[mid] < target){
+                l = mid+1;
+            }
+            else if(nums[mid] > target){
+                h = mid-1;
+            }
         }
         
+         l = 0;
+         h = nums.length-1;
         
-        l=0;
-        h=nums.length-1;
-        
-        while(l<=h){
+        while(l <= h){
+            int mid = l + (h-l)/2;
             
-            int mid=(l+h)/2;
-            
-            if(nums[mid]==target){
-                ans[1]=mid;
-                l=mid+1;
-                
-            }
-            else if(nums[mid]<target){
-                l=mid+1;
-            }
-            else {
-                h=mid-1;
+            if(nums[mid] == target){
+                ans[1] = mid;
+                l = mid+1;
             }
             
+            else if(nums[mid] < target){
+                l = mid+1;
+            }
+            else if(nums[mid] > target){
+                h = mid-1;
+            }
         }
-        
-        
         return ans;
-        
     }
 }
