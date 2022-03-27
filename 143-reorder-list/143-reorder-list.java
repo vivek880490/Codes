2818fe73eccd9018d36512624b90b283239ca683
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-27 12:16:58 +0530

    Time: 2 ms (69.21%), Space: 54.6 MB (20.80%) - LeetHub

--- 143-reorder-list/143-reorder-list.java
+++ 143-reorder-list/143-reorder-list.java
@@ -10,23 +10,23 @@
  */
 class Solution {
     public void reorderList(ListNode head) {
-        if(head==null || head.next==null) return;
         
+        if(head == null || head.next == null) return;
         
-        ListNode midd=mid(head);
-        ListNode nhead=midd.next;
-        midd.next=null;
+        ListNode midd = mid(head);
+        ListNode nhead = midd.next;
+        midd.next = null;
         
-        nhead=reverse(nhead);
+        nhead = reverse(nhead);
         
         ListNode c1=head;
         ListNode c2=nhead;
         ListNode f1=null;
         ListNode f2=null;
         
-        while(c1!=null && c2!=null){
-            //backup
-            
+        
+        
+        while(c1 != null && c2 != null){
             f1=c1.next;
             f2=c2.next;
             
@@ -39,40 +39,35 @@ class Solution {
             c1=f1;
             c2=f2;
         }
-       
-     
+        
         
     }
     
     ListNode mid(ListNode head){
-        if(head==null || head.next==null) return head;
+        if(head == null || head.next == null) return head;
         
-        ListNode slow=head;
-        ListNode fast=head;
+        ListNode slow = head;
+        ListNode fast = head;
         
-        while(fast.next!=null && fast.next.next!=null){
-            slow=slow.next;
-            fast=fast.next.next;
+        while(fast.next != null && fast.next.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
         }
+        
         return slow;
+        
     }
     
     ListNode reverse(ListNode head){
+        if(head == null || head.next == null) return head;
+        ListNode cur = null;
+        ListNode temp = head;
         
-        if(head==null || head.next==null) return head;
-        
-        ListNode cur=null;
-        ListNode temp=head;
-        
-        while(temp!=null){
-            ListNode forward=temp.next; // backup
-            
-            temp.next=cur; // link;
-            //now move
-            
-            cur=temp;
-            temp=forward;
-            
+        while(temp != null){
+            ListNode next = temp.next;
+            temp.next = cur;
+            cur = temp;
+            temp = next;
         }
         return cur;
     }
