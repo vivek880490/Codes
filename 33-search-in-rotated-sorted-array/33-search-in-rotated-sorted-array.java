4be7544df76516d48d6ea3f5bc2b4aaf44c78f0d
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-28 11:13:42 +0530

    Time: 1 ms (68.63%), Space: 42.5 MB (54.51%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,30 +1,56 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        int l=0;
-        int h=n-1;
+        int n = nums.length;
         
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(nums[mid]==target) return mid;
+        //if(n == 1 && nums[0]==target) return 0;
+        
+        int index = indexofminelemnt(nums);
+        
+        int s1 = binarysearch(nums,0,index-1,target);
+        int s2 = binarysearch(nums, index, n-1, target);
+        
+        return s1==-1?s2:s1;
+       
+        
+    }
+    
+    int indexofminelemnt(int nums[]){
+        int n = nums.length;
+        int l = 0;
+        int h = n-1;
+        
+        while(l <= h){
+            if(nums[l] < nums[h]) return l;
             
-            if(nums[l]<=nums[mid]){
-                if(target>=nums[l] && target<=nums[mid]){
-                    h=mid-1;
-                }
-                else{
-                    l=mid+1;
-                }
+            int mid = l + (h-l)/2;
+            
+            int left = (mid+n-1) % n;
+            int right = (mid+1) % n;
+            
+            if(nums[left] >= nums[mid] && nums[right] >= nums[mid]){
+                return mid;   
             }
-            else{
-                if(target>=nums[mid] && target<=nums[h]){
-                    l=mid+1;
-                }
-                else{
-                    h=mid-1;
-                }
+            
+            else if(nums[l] <= nums[mid]){
+                l = mid+1;
+            }
+            else if(nums[h] >= nums[mid]){
+                h = mid - 1;
             }
         }
+        
+        return 0;
+    }
+    
+    int binarysearch(int nums[],int l, int h, int x){
+        
+        while(l <= h){
+        int mid = l +(h-l)/2;
+        if(nums[mid] == x) return mid;
+        if(nums[mid] < x) l = mid+1;
+        else if(nums[mid] > x) h = mid-1;
+        }
         return -1;
+        
     }
 }
