b9e41ac41285c9e24836660d5f5b705e6dc29e97
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-30 09:47:02 +0530

    Time: 891 ms (8.52%), Space: 150.3 MB (8.05%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -1,42 +1,39 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String s1=s;
+        
         StringBuilder sb = new StringBuilder(s);
+        
         sb.reverse();
-        s = sb.toString();
-    
-        String ans="";
-       int n=s.length();
-        int res=0;
-      int dp[][] = new int[n+1][n+1];
-        for(int i=0;i<=n;i++){
-            for(int j=0;j<=n;j++){
-                if(i==0||j==0)
-                    dp[i][j]=0;
-               else if(s.charAt(i-1)==s1.charAt(j-1)){
-                    dp[i][j]=dp[i-1][j-1]+1;
-                   
-                   
+        String t = sb.toString();
+        
+        
+        int dp[][] = new int[s.length()+1][s.length()+1];
+        int res = 0;
+        String ans = "";
+        
+        for(int i=1; i<dp.length; i++){
+            for(int j=1; j<dp[0].length; j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
                 }
-                else
-                    dp[i][j]=0;
+                else{
+                    dp[i][j] = 0;
+                }
                 
-                 if(dp[i][j]>res)
-                    {
-                        String temp=s.substring(i-dp[i][j],i-dp[i][j]+dp[i][j]);
-                        
-                     StringBuilder sb1 = new StringBuilder(temp);
-                     sb1.reverse();
-                     String rev = sb1.toString();
-                        if(temp.equals(rev)){    //check if common substring is palindromic or not
-                             ans=temp;
-                        res=dp[i][j];
-                        }
-                           
+                if(res < dp[i][j]){
+                  String temp = s.substring(i-dp[i][j], i-dp[i][j]+dp[i][j]);
+                    StringBuilder rev = new StringBuilder(temp);
+                    rev.reverse();
+                    String reve = rev.toString();
+                    
+                    if(reve.equals(temp)){
+                        ans = temp;
+                        res = dp[i][j];
                     }
+                }
             }
-            
         }
         return ans;
+        
     }
 }
