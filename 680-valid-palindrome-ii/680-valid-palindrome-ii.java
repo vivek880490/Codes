b45bc455526049f0b61383fdf1a62bae4e2baf0a
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-03-30 19:59:02 +0530

    Time: 13 ms (29.46%), Space: 54.2 MB (71.27%) - LeetHub

---
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean validPalindrome(String s) {
+        int i = 0;
+        int j = s.length()-1;
+        
+        while(i < j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return (valid(s,i+1,j) || valid(s,i,j-1));
+            }
+        }
+        return true;
+    }
+    
+    boolean valid(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) == s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
