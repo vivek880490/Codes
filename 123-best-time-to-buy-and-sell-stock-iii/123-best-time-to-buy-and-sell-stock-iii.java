a3a63c11d920c43e1f7fb197f033575b10e625a4
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-04-02 23:37:07 +0530

    Time: 206 ms (6.49%), Space: 124.7 MB (10.26%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.java
@@ -1,25 +1,23 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
-        Integer qb[][][] = new Integer[n][2][3];
-        return solve(0, prices, 1 , 2, qb);
+        int qb[][][] = new int[n+1][2][3];
         
-    }
-    int solve(int start, int prices[], int buy, int allow,Integer qb[][][]){
-        if(start == prices.length || allow == 0) return 0;        
-        int profit = 0;
-        
-        if(qb[start][buy][allow] != null) return qb[start][buy][allow];
-        
-        if(buy == 1){
-        profit = Math.max(-prices[start] + solve(start+1, prices, 0, allow, qb),solve(start+1, prices, 1, allow, qb)); 
+        for(int start = n-1; start>=0; start--){
+            for(int buy=0; buy<=1; buy++){
+                for(int allow=1; allow<=2; allow++){
+                    
+                    if(buy == 1){
+                      qb[start][buy][allow] = Math.max(-prices[start] + qb[start+1][0] [allow],qb[start+1][1][allow]);   
+                    }
+                    else{
+                      qb[start][buy][allow] = Math.max(prices[start] + qb[start+1][1] [allow-1],qb[start+1][0][allow]);     
+                    }
+                }
             }
-        
-        else{
-            profit = Math.max(prices[start] + solve(start+1, prices, 1, allow-1,qb),solve(start+1, prices, 0, allow, qb));
-            
         }
         
-        return qb[start][buy][allow] = profit;
+        return qb[0][1][2];
+        
     }
 }
