4dacc6985d66456f3493f70fa2613e7b45c30f56
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-04-07 09:16:06 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (59.97%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,18 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
         
-        Integer qb[] = new Integer[n+1];
+        return dp[n];
+//         if(n == 0) return 1;
+//         if(n == 1) return 1;
         
-        return solve(n,qb);
-    }
-    
-    int solve(int n, Integer qb[]){
-        if(n == 0 || n == 1) return 1;
-        
-        if(qb[n] != null) return qb[n];
-        
-        int ans = solve(n-1,qb) + solve(n-2,qb);
-        
-        return qb[n]=ans;
+//         return climbStairs(n-1)+climbStairs(n-2);
     }
 }
