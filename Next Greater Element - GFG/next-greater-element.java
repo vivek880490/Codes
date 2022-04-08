4e4f7d77f3479c99b4ea6caf8a1cd9d14aa107ae
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-04-08 16:10:39 +0530

    Added solution - LeetHub

--- Next Greater Element - GFG/next-greater-element.java
+++ Next Greater Element - GFG/next-greater-element.java
@@ -42,22 +42,22 @@ class Solution
             if(st.size() == 0){
                 ans[i] = -1;
             }
-            else if(st.size() > 0 && st.peek() > arr[i]){
+            else if(st.size() > 0 && st.peek() >= arr[i]){
                 ans[i] = st.peek();
-            } 
-            else if(st.size() > 0 && st.peek() <= arr[i]){
                 
-                while(st.size() > 0 && st.peek() <= arr[i]){
-                    st.pop();
-                }
-                if(st.size() == 0){
-                    ans[i] = -1;
-                }
-                else{
-                    ans[i] = st.peek();
-                }
             }
-            
+            else if(st.size() > 0 && st.peek() <= arr[i]){
+                 while(st.size()>0 && st.peek() <= arr[i]){
+                     st.pop();
+                 }
+                 if(st.size() == 0){
+                     ans[i] = -1;
+                 }
+                 else{
+                     ans[i] = st.peek();
+                 }
+                 
+            }
             st.push(arr[i]);
         }
         
