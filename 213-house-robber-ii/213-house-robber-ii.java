b28a537ce9210ca2b89a8b8027d849d78a75121a
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-04-08 09:27:47 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (92.51%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,27 +1,25 @@
 class Solution {
     public int rob(int[] nums) {
         
-        if(nums.length == 1) return nums[0];
-        
-        Integer qb1[] = new Integer[nums.length+1];
-        Integer qb2[] = new Integer[nums.length+1];
         int n = nums.length;
-        int s1 = solve1(nums, 0,n-1, qb1);
-        int s2 = solve1(nums, 1,n, qb2);
+        if(n == 1) return nums[0];
+        Integer qb1[] = new Integer[n+1];
+        Integer qb2[] = new Integer[n+1];
+        int s1 = solve(0, n-1, nums, qb1);
+        int s2 = solve(1, n, nums, qb2);
+        return Math.max(s1,s2);
         
-        return Math.max(s1, s2);
     }
     
-    int solve1(int nums[], int i,int n, Integer qb[]){
-        if(i >= n) return 0;
-        if(qb[i] != null) return qb[i];
-        int rob = nums[i] + solve1(nums,i+2,n,qb);
-        int skip = solve1(nums, i+1, n, qb);
+    int solve(int start, int n, int nums[], Integer qb[]){
         
-        int temp = Math.max(rob, skip);
+        if(start >= n) return 0;
         
-        return qb[i] = temp;
+        if(qb[start] != null) return qb[start];
+        
+        int loot = nums[start] + solve(start+2, n, nums, qb);
+        int skip = solve(start+1,n, nums, qb);
+        
+        return qb[start] = Math.max(loot, skip);
     }
-    
-    
 }
