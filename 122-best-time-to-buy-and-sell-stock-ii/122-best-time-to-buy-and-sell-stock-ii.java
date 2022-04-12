18e1a5760bd3597fcd32923a727244e373b58e61
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-04-12 12:04:50 +0530

    Time: 10 ms (5.56%), Space: 50.2 MB (5.68%) - LeetHub

--- 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,37 +1,24 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        
-        
-        // agar buy ka option hai karenge to paisa lagega, -prices[i], aur buy kar liye to
-        //next call aur buy ka option nahi rahega, 
-        // ya naya buy nahi karenge profit = 0, buy ka option khul jayega
-        
-        
-        // else sell karenge to paisa aayega +prices[i], aur sell kar diye to next buy ka option khul jayega, sell nahi karenge to profit 0 aur next buy ka option band rahega
-        
         int n = prices.length;
-        Integer qb[][] = new Integer[n+1][2];
+        Integer qb[][] = new Integer[n][2];
+        return solve(0, 1, prices,qb);
+    }
+    
+    int solve(int start, int buy, int prices[],Integer qb[][]){
+        if(start == prices.length) return 0;
+        if(qb[start][buy] != null) return qb[start][buy];
         
-        qb[n][0] = 0;
-        qb[n][1] = 0;
-        
-        for(int start=n-1; start>=0; start--){
-            for(int buy=0; buy<=1; buy++){
-                int profit = 0;
-        
-       
+        int profit = 0;
         
         if(buy == 1){
-            profit = Math.max(-prices[start] + qb[start+1][0], 0 + qb[start+1][1]);
-        }
-        else{
-            profit = Math.max(prices[start] + qb[start+1][1], 0 + qb[start+1][0]);
-        }
-        
-        qb[start][buy] = profit;
-            }
-        }
-    
-       return qb[0][1];
+            profit = Math.max(-prices[start]+solve(start+1, 0, prices,qb), 0 + solve(start+1, 1, prices,qb));
     }
+        
+    else{
+        profit = Math.max(prices[start] + solve(start+1, 1, prices,qb), 0+solve(start+1, 0, prices,qb));
+    }
+        
+        return qb[start][buy] = profit;
+}
 }
