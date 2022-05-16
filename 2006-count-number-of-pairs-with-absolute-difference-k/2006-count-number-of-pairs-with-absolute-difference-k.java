d6cc9ad9cc5035fff0145e441f21b4e920852f45
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-05-16 23:41:34 +0530

    Time: 7 ms (64.41%), Space: 44.6 MB (18.42%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.java
@@ -1,17 +1,22 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
+        
+        HashMap<Integer,Integer> map = new HashMap<>();
         int count = 0;
-        for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                if(i!=j){
-                    int temp = Math.abs(nums[i] - nums[j]);
-                    if(temp == k){
-                        count++;
-                    }
-                }
+        for(int x : nums){
+            if(map.containsKey(x+k)){
+                count += map.get(x+k);
             }
+            
+            if(map.containsKey(x-k)){
+                count += map.get(x-k);
+            }
+            
+            map.put(x, map.getOrDefault(x,0)+1);
         }
         
         return count;
+        
+        
     }
 }
