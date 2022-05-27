04903db7bb575ad850b9e668d3aa7766a591787a
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-05-27 10:24:36 +0530

    Time: 26 ms (55.68%), Space: 60.9 MB (72.68%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.java
+++ 743-network-delay-time/743-network-delay-time.java
@@ -1,50 +1,19 @@
 class Solution {
-    
-    public int networkDelayTime(int[][] times, int n, int k) {
-        
-        ArrayList<ArrayList<int[]>> al = new ArrayList<>();
-        
-        for(int i=0; i<=n; i++){
-            al.add(new ArrayList<int[]>());
+    public int networkDelayTime(int[][] times, int N, int K) {
+    int[] disTo = new int[N];
+    Arrays.fill(disTo, Integer.MAX_VALUE);
+    disTo[K - 1] = 0;
+    for (int i = 1; i < N; i++) {
+        for (int[] edge : times) {
+            int u = edge[0] - 1, v = edge[1] - 1, w = edge[2];
+            if(disTo[u] == Integer.MAX_VALUE) continue;
+            disTo[v] = Math.min(disTo[v], disTo[u] + w);
         }
-        
-        for(int x[] : times){
-            al.get(x[0]).add(new int[]{x[1],x[2]});
-        }
-        
-        boolean visited[] = new boolean[n+1];
-        
-        int ans = solve(al, visited, k);
-        
-        for(int i=1; i<=n; i++){
-            if(visited[i] == false) return -1;
-        }
-        
-        return ans;
     }
-    
-    int solve( ArrayList<ArrayList<int[]>>al, boolean visited[], int k){
-        
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[1]-b[1]));
-        
-        pq.add(new int[]{k,0});
-        
-        int mst = 0;
-        
-        while(pq.size() > 0){
-            int rem[] = pq.poll();
-            
-            if(visited[rem[0]] == true) continue;
-            visited[rem[0]] = true;
-            
-            mst = Math.max(mst, rem[1]);
-            
-            for(int x[] : al.get(rem[0])){
-                
-                pq.add(new int[]{x[0],x[1]+rem[1]});
-            }
-        }
-        
-        return mst;
+    int res = Integer.MIN_VALUE;
+    for (int i: disTo) {
+        res = Math.max(i, res);
     }
+    return res == Integer.MAX_VALUE ? -1 : res;
+}
 }
