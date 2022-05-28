631f80a7c41ecfe359580167eccc71f9d921757f
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-05-28 23:04:04 +0530

    Time: 8 ms (25.83%), Space: 49.6 MB (99.59%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,29 +1,38 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int dirs[][] = {{0,1},{1,0},{0,-1},{-1,0}};
+        
+        Queue<int[]> q = new LinkedList<>();
         
         boolean vis[][] = new boolean[grid.length][grid[0].length];
+        
         int count = 0;
         for(int i=0; i<grid.length; i++){
             for(int j=0; j<grid[0].length; j++){
                 if(grid[i][j] =='1' && vis[i][j] == false){
-                    dfs(grid,i,j,vis);
+                    q.add(new int[]{i,j});
                     count++;
+                    vis[i][j] = true;
+             
+        
+        while(q.size() > 0){
+            int rem[] = q.poll();
+            
+            for(int k=0; k<4; k++){
+                int dx = rem[0] + dirs[k][0];
+                int dy = rem[1] + dirs[k][1];
+                
+                
+                if(dx >= 0 && dy >=0 && dx < grid.length && dy < grid[0].length && vis[dx][dy] == false && grid[dx][dy] == '1'){
+                    vis[dx][dy] = true;
+                    q.add(new int[]{dx,dy});
                 }
+                
+            }
+        }
+                       }
             }
         }
         return count;
-        
-    }
-    
-    void dfs(char grid[][], int i, int j,boolean vis[][]){
-        
-        if(i<0 || j<0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0' || vis[i][j]==true) return;
-        
-        vis[i][j] = true;
-        
-        dfs(grid,i+1,j,vis);
-        dfs(grid,i,j+1,vis);
-        dfs(grid,i-1,j,vis);
-        dfs(grid,i,j-1,vis);
     }
 }
