10a765f92c5ecf10b27a88b09eafbe611f0e4545
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-05-28 12:11:04 +0530

    Time: 9 ms (45.51%), Space: 58 MB (58.35%) - LeetHub

---
+++ 690-employee-importance/690-employee-importance.java
@@ -0,0 +1,28 @@
+/*
+// Definition for Employee.
+class Employee {
+    public int id;
+    public int importance;
+    public List<Integer> subordinates;
+};
+*/
+
+class Solution {
+    public int getImportance(List<Employee> employees, int id) {
+        int total = 0;
+        Map<Integer, Employee> map = new HashMap<>();
+        for (Employee employee : employees) {
+            map.put(employee.id, employee);
+        }
+        Queue<Employee> queue = new LinkedList<>();
+        queue.offer(map.get(id));
+        while (!queue.isEmpty()) {
+            Employee current = queue.poll();
+            total += current.importance;
+            for (int subordinate : current.subordinates) {
+                queue.offer(map.get(subordinate));
+            }
+        }
+        return total;
+    }
+}
