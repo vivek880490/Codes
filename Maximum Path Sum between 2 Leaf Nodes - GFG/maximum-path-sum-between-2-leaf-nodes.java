2c635970eda4fa30645b8d2a33c5dd3f73e6f693
Vivek Kumar <54447269+vivek880490@users.noreply.github.com>
2022-05-30 23:30:17 +0530

    Added solution - LeetHub

--- Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.java
+++ Maximum Path Sum between 2 Leaf Nodes - GFG/maximum-path-sum-between-2-leaf-nodes.java
@@ -120,13 +120,20 @@ class Solution
     int sum=Integer.MIN_VALUE;
        int findMaxPathSum(Node root){
            if(root==null)return 0;
+           
            int lsum=findMaxPathSum(root.left);
+           
            int rsum=findMaxPathSum(root.right);
+           
            if(root.left== null && root.right==null)return root.data;
+           
            if(root.left==null) return root.data+ rsum;
+           
            if(root.right==null) return root.data+lsum;
-           sum=Math.max(sum, root.data+lsum+rsum);
-           return Math.max(root.data+lsum, root.data+rsum);
+           int temp = Math.max(root.data+lsum, root.data+rsum);
+           sum = Math.max(sum, root.data+lsum+rsum);
+           
+           return temp;
            
        }
        
